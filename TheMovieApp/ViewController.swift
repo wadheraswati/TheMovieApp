6ce0d9b3b8916d20bf90fecdc5a54422bc26a013
Swati Wadhera <wadheraswati@gmail.com>
2019-09-02 17:32:29 +0530

    Initial Commit

---
+++ TheMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieApp
+//
+//  Created by Swati Wadhera on 02/09/19.
+//  Copyright © 2019 Swati Wadhera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
