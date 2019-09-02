7919e453ff80ac48776bc5bf2f3df956e0a218e9
Swati Wadhera <wadheraswati@gmail.com>
2019-09-03 00:18:27 +0530

    added api to get movie detail and cast

---
+++ TheMovieApp/ViewModel/MovieDetailVM.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDetailVM.swift
+//  TheMovieApp
+//
+//  Created by Swati Wadhera on 03/09/19.
+//  Copyright © 2019 Swati Wadhera. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailVM: NSObject {
+
+}
