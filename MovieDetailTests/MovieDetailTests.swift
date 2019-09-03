8ba1e811c81d09f26e3c9d470062dbfe5fcc8b53
Swati Wadhera <wadheraswati@gmail.com>
2019-09-03 10:03:52 +0530

    added pods for cachable

--- MovieDetailTests/MovieDetailTests.swift
+++ MovieDetailTests/MovieDetailTests.swift
@@ -6,28 +6,67 @@
 //  Copyright © 2019 Swati Wadhera. All rights reserved.
 //
 
+
 import XCTest
+@testable import TheMovieApp
 
 class MovieDetailTests: XCTestCase {
-
+    
+    var sut : MovieDetailVC!
+    
     override func setUp() {
+        sut = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MovieDetailVCIdentifier") as? MovieDetailVC
+        sut.loadViewIfNeeded()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testNotNilMovieId() {
+        sut = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MovieDetailVCIdentifier") as? MovieDetailVC
+        XCTAssertNil(sut.movieId)
+        sut.movieId = 1
+        sut.loadViewIfNeeded()
+        sut.viewDidLayoutSubviews()
+    }
+    
+    func testMovieDetails() {
+        sut.movieId = 420818
+        sut.getMovieDetail()
+    }
+    
+    func testNotExistingMovieDetails() {
+        sut.movieId = 1
+        sut.getMovieDetail()
+    }
+    
+    func testDummyNilMovieDetail() {
+        sut.movieDetail = MovieDetail(id: 1, title: "", vote_average: 0, backdrop_path: nil, release_date: "", spoken_languages: nil, genres: nil, vote_count: 0, runtime: 0, overview: nil)
+        sut.setupUI()
+        sut.setUpCastLbl(withCast: nil)
+        
+    }
+    
+    func testRandomDummyData() {
+        sut.movieDetail = MovieDetail(id: 1, title: "Movie Title", vote_average: 4.6, backdrop_path: nil, release_date: "24324", spoken_languages: [], genres: [], vote_count: 0, runtime: 60, overview: nil)
+        sut.setupUI()
+        sut.setUpCastLbl(withCast: nil)
+    }
+    
+    func testCastDummyData() {
+        sut.movieDetail = MovieDetail(id: 1, title: "Movie Title", vote_average: 4.6, backdrop_path: nil, release_date: "24324", spoken_languages: [], genres: [], vote_count: 500, runtime: 61, overview: nil)
+        sut.setupUI()
+        sut.setUpCastLbl(withCast: [MovieCast(character: "New", name: "Name")])
+    }
+    
+    func testVoteCountWithVoteAverage() {
+        sut.movieDetail = MovieDetail(id: 1, title: "Movie Title", vote_average: 0, backdrop_path: nil, release_date: "24324", spoken_languages: [], genres: [], vote_count: 500, runtime: 160, overview: nil)
+        sut.setupUI()
+        sut.setUpCastLbl(withCast: [MovieCast(character: "New", name: "Name")])
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
--- MovieDetailUITests/MovieDetailUITests.swift
+++ MovieDetailUITests/MovieDetailUITests.swift
@@ -22,9 +22,9 @@ class MovieDetailUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    
+    
+    
 
     func testExample() {
         // Use recording to get started writing UI tests.
